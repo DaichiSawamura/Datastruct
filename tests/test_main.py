20164770a8a3da9596583bd03d1ace96eadb3fc4
DaichiSawamura <dimakr-303@yandex.ru>
2023-03-01 18:44:54 +0300

    add

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 import unittest
 from main import *
+from custom_queue import *
 
 n1 = Node(5, None)
 n2 = Node('a', n1)
@@ -10,6 +11,11 @@ stack.push('data2')
 stack.push('data3')
 
 stack.pop()
+
+queue = Queue()
+queue.enqueue('data1')
+queue.enqueue('data2')
+queue.enqueue('data3')
 
 
 class TestMain(unittest.TestCase):
@@ -26,3 +32,9 @@ class TestMain(unittest.TestCase):
 
     def test_pop(self):
         self.assertEqual(stack.top.data, 'data2')
+
+    def test_Queue(self):
+        self.assertEqual(queue.head.data, 'data1')
+        self.assertEqual(queue.head.next_node.data, 'data2')
+        self.assertEqual(queue.tail.data, 'data3')
+        self.assertEqual(queue.tail.next_node, None)
