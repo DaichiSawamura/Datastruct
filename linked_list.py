f93114432b7861190c57161a0abf40d2dec60cc3
DaichiSawamura <dimakr-303@yandex.ru>
2023-03-14 16:29:18 +0300

    add get_data_by_id and to_list

--- linked_list.py
+++ linked_list.py
@@ -40,10 +40,42 @@ class LinkedList:
         ll_string += ' None'
         print(ll_string)
 
+    def to_list(self):
+        lst = []
+        node = self.head
+        while node:
+            lst.append(node.data)
+            node = node.next_node
+        return lst
+
+    def get_data_by_id(self, id):
+        node = self.head
+        try:
+            while node:
+                if node.data['id'] == id:
+                    return node.data
+                node = node.next_node
+            return None
+        except TypeError:
+            print("Данные не являются словарем или в словаре нет id")
+
+
+#ll = LinkedList()
+#ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+#ll.insert_at_end({'id': 2, 'username': 'mik.roz'})
+#ll.insert_at_end({'id': 3, 'username': 'mosh_s'})
+#ll.insert_beginning({'id': 0, 'username': 'serebro'})
+
+#lst = ll.to_list()
+#for item in lst: print(item)
+
+#user_data = ll.get_data_by_id(3)
+#print(user_data)
 
 ll = LinkedList()
-ll.insert_beginning({'id': 1})
-ll.insert_at_end({'id': 2})
-ll.insert_at_end({'id': 3})
-ll.insert_beginning({'id': 0})
-ll.print_ll()
+ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+ll.insert_at_end('idusername')
+ll.insert_at_end([1, 2, 3])
+ll.insert_at_end({'id': 2, 'username': 'mosh_s'})
+user_data = ll.get_data_by_id(2)
+print(user_data)
